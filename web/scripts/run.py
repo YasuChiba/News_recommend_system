8219f7bd77a104b2d7538bc63e7e75a9d13cdba9
YasuChiba <yasu.bv0309@gmail.com>
2020-08-08 00:35:33 +0900

    vue追加

--- web/scripts/run.py
+++ web/scripts/run.py
@@ -1,19 +1,21 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app():
 
-  app = Flask(__name__)
+  app = Flask(__name__, static_folder='./frontend/dist/static', template_folder='./frontend/dist')
 
 
   return app
 
 app = create_app()
 
-@app.route("/")
-def index():
-    return "Hello World!!"
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return render_template('index.html')
 
 
 @app.route("/test")
