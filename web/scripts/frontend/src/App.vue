0398f5d55b14be037e97b19367e986c101808c36
YasuChiba <yasu.bv0309@gmail.com>
2020-08-08 00:57:09 +0900

    add pages

--- web/scripts/frontend/src/App.vue
+++ web/scripts/frontend/src/App.vue
@@ -2,6 +2,14 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <el-menu :default-active="activeIndex" mode="horizontal" router>
+      <el-menu-item index="home" :route="{ name:'home' }">Home</el-menu-item>
+      <el-menu-item index="about" :route="{ name:'about' }">About</el-menu-item>
+      <el-menu-item>
+        <a href="https://element.eleme.io" target="_blank">Link</a>
+      </el-menu-item>
+    </el-menu>
+    <router-view />
   </div>
 </template>
 
@@ -12,6 +20,11 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      activeIndex: this.$route.name
+    }
   }
 }
 </script>
---
+++ web/scripts/frontend/src/views/Home.vue
@@ -0,0 +1,12 @@
+frontend/src/views/Home.vue
+<template>
+  <div class="home">
+    <h1>This is a home page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
