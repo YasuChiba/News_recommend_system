9bb8d2ac95e9b1c0681ece543527d10cfa19d1b6
YasuChiba <yasu.bv0309@gmail.com>
2020-08-08 19:27:32 +0900

    いろいろついか

--- web/scripts/frontend/src/views/Annotating.vue
+++ web/scripts/frontend/src/views/Annotating.vue
@@ -2,9 +2,22 @@
   <div class="home">
     <h1>This is a annotating page</h1>
     <el-table class="data-table" :data="tableData" stripe>
-     <el-table-column prop="id" label="ID" width="180"></el-table-column>
-     <el-table-column prop="name" label="名前" width="180"></el-table-column>
-     <el-table-column prop="note" label="備考"></el-table-column>
+      <el-table-column prop="id" label="ID" width="100"></el-table-column>
+      <el-table-column prop="og_site_name" label="サイト名" width="140"></el-table-column>
+      <el-table-column prop="og_title" label="Title"></el-table-column>
+      <el-table-column prop="og_description" label="text"></el-table-column>
+      <el-table-column label="URL">
+        <template slot-scope="scope">
+            <el-link :href="scope.row.url" target="_blank">link</el-link>
+        </template>
+      </el-table-column>
+      <el-table-column label="カテゴリ">
+        <template>
+            <el-checkbox v-model="checked1" label="Option1" border></el-checkbox>
+            <el-checkbox v-model="checked2" label="Option2" border></el-checkbox>
+        </template>
+      </el-table-column>
+
     </el-table>
   </div>
 </template>
@@ -20,11 +33,11 @@ export default {
     }
   },
   mounted () {
-    this.updataTableData()
+    this.updateTableData()
   },
   methods: {
-    updataTableData: async function () {
-      const response = await axios.get('/test')
+    updateTableData: async function () {
+      const response = await axios.get('api/test2')
       console.log(response)
       this.tableData = response.data
     }
